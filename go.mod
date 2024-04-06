bcc368d552bab1a0fd800a5388f42d7c92b2c5bb
Kwitsch <kwitsch@gmail.com>
2024-04-06 00:40:17 +0000

    added watchdog

--- go.mod
+++ go.mod
@@ -5,10 +5,16 @@ go 1.22.2
 require github.com/kwitsch/go-dockerutils v0.0.9
 
 require (
+	github.com/stretchr/testify v1.7.1 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
+
+require (
 	github.com/creasty/defaults v1.5.2 // indirect
 	github.com/knadh/koanf v1.3.3 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/ramr/go-reaper v0.2.1 // indirect
+	github.com/u-root/u-root v0.14.0
 )
--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/signal"
 	_ "time/tzdata"
+
+	"github.com/kwitsch/RPIWatchdog/config"
+	"github.com/u-root/u-root/pkg/watchdog"
 
 	_ "github.com/kwitsch/go-dockerutils"
 )
 
 func main() {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Printf("Error getting config: %v", err)
+		os.Exit(1)
+	}
+
+	wd, err := watchdog.Open(cfg.DevicePath)
+	if err != nil {
+		log.Printf("Error opening watchdog: %v", err)
+		os.Exit(2)
+	}
+	defer wd.Close()
+
 	intChan := make(chan os.Signal, 1)
 	signal.Notify(intChan, os.Interrupt)
 	defer close(intChan)
@@ -16,7 +33,13 @@ func main() {
 	for {
 		select {
 		case <-intChan:
+			wd.MagicClose()
 			os.Exit(0)
+		default:
+			if err := wd.KeepAlive(); err != nil {
+				log.Printf("Error keeping watchdog alive: %v", err)
+				os.Exit(4)
+			}
 		}
 	}
 }
